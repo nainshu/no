404d2bb9232f335747f6905a9c825d0f2f61b9cf
nainshu <2087819028@qq.com>
2024-10-26 18:05:54 +0800

    Update APT.lua

--- APT.lua
+++ APT.lua
@@ -22,7 +22,7 @@ local about = Window:MakeTab({
 
 about:AddParagraph("您的用户名:"," "..game.Players.LocalPlayer.Name.."")
 about:AddParagraph("您的注入器:"," "..identifyexecutor().."")
-about:AddParagraph("官方Q群"," "..599298896.."")
+about:AddParagraph("您当前服务器的ID"," "..game.GameId.."")
 
 local Tab = Window:MakeTab({
   Name = "DOORS脚本",
