c09a6a8977bde6fc5121589e18c09d29224c46dd
nainshu <2087819028@qq.com>
2024-10-29 01:06:48 +0800

    Update APT.lua

--- APT.lua
+++ APT.lua
@@ -30,6 +30,66 @@ local Tab = Window:MakeTab({
   PremiumOnly = false
   })
 Tab:AddButton({
+	Name = "小恶魔玩具",
+	Callback = function()
+
+local Players = game:GetService("Players")
+local Equipped = false
+local Plr = Players.LocalPlayer
+local Char = Plr.Character or Plr.CharacterAdded:Wait()
+local Hum = Char:WaitForChild("Humanoid")
+local Root = Char:WaitForChild("HumanoidRootPart")
+local RightArm = Char:WaitForChild("RightUpperArm")
+local LeftArm = Char:WaitForChild("LeftUpperArm")
+local RightC1 = RightArm.RightShoulder.C1
+local LeftC1 = LeftArm.LeftShoulder.C1
+local CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/MateiDaBest/Utilities/main/Doors/Custom%20Shop%20Items/Main.lua"))()
+local Goblino = game:GetObjects("rbxassetid://12856605563")[1]
+
+Goblino.Parent = game.Players.LocalPlayer.Backpack
+
+CustomShop.CreateItem({
+	Title = "El Goblino",
+	Desc = "Door 52",
+	Image = "https://cdn.discordapp.com/attachments/970078853127110677/1087774910706888824/12372522258.png",
+	Price = 52,
+	Stack = 1,
+})
+
+local function setupHands(tool)
+	tool.Equipped:Connect(function()
+		Equipped = true
+		Char:SetAttribute("Hiding", true)
+		for _, v in next, Hum:GetPlayingAnimationTracks() do
+			v:Stop()
+		end
+
+		RightArm.Name = "R_Arm"
+		LeftArm.Name = "L_Arm"
+
+		RightArm.RightShoulder.C1 = RightC1
+			* CFrame.Angles(math.rad(-90), math.rad(-10), 0)
+		LeftArm.LeftShoulder.C1 = LeftC1
+			* CFrame.new(-0.2, -0.4, -0.5)
+			* CFrame.Angles(math.rad(-110), math.rad(30), math.rad(0))	
+	end)
+
+	tool.Unequipped:Connect(function()
+		Equipped = false
+		Char:SetAttribute("Hiding", nil)
+		RightArm.Name = "RightUpperArm"
+		LeftArm.Name = "LeftUpperArm"
+
+		RightArm.RightShoulder.C1 = RightC1
+		LeftArm.LeftShoulder.C1 = LeftC1
+	end)
+end
+
+setupHands(Goblino)
+
+end
+})
+Tab:AddButton({
 	Name = "万圣节糖果透视",
 	Callback = function()
 
