76988f207eed54ee1c4227287547de182be461bf
nainshu <2087819028@qq.com>
2024-11-07 21:01:21 +0800

    Update APT.lua

--- APT.lua
+++ APT.lua
@@ -67,6 +67,40 @@ local Tab = Window:MakeTab({
   PremiumOnly = false
   })
 Tab:AddButton({
+	Name = "生成giggle",
+	Callback = function()
+
+getgenv().GIGGLE_SPAWN_CONFIG = {
+     Damage = 6,
+     AttackingTime = math.random(7, 10), -- The time giggle will be attacking for.
+     FallSpeed = 3,        -- Speed for when giggle spawns, can be mininum 2 and how high you want
+     
+     Stunnable = true,    -- If set to true, Giggle will be stunnable with glowstick
+     StunTime = 5, -- The time giggle will be stun for
+     
+     RagdollThrowForce = 50,    -- The Ragdoll's Force when its thrown
+     RagdollDissapears = true,  -- If set to true the ragdoll will dissapear once giggle finishes attacking.
+  
+     RoomSpawning = {
+          Enabled = true   -- If set to false, giggle will spawn around the player.
+     },
+     
+     PlayerSpawning = { -- This table will be used if Room Spawning is Disabled
+          MinRadius = -20,  -- The minimum distance giggle can spawn from the player.
+          MaxRadius = 20.  --  The maximum distance giggle can spawn from the player.
+     },
+     
+     SpawningKey = { -- Key for Spawning
+          Enabled = false,  -- If enabled once the key is pressed, giggle will spawn.
+          Key = "G"    -- Key that is used for spawning giggle
+     }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Test/refs/heads/main/Doors/GiggleSpawn.lua"))()
+
+end
+})
+Tab:AddButton({
 	Name = "金色手摇",
 	Callback = function()
 
