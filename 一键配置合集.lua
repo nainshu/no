75b3a76f14869ea509fa7659de1ff249103cb448
nainshu <2087819028@qq.com>
2025-01-27 19:12:55 +0800

    Add files via upload

---
+++ 一键配置合集.lua
@@ -0,0 +1,38 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/coast%20old"))()
+local test = Library:CreateTab("快捷配置修饰符", "Description")
+
+test:CreateButton("外网小恶魔矿井150%修饰符", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nainshu/no/main/Imp%20modifier.lua"))()
+end)
+
+test:CreateButton("外网小恶魔矿井150%修饰符（多人）", function()
+    local args = {
+    [1] = {
+        ["Mods"] = {
+            [1] = "EntitiesMore",
+            [2] = "RoomsA90",
+            [3] = "ItemSpawnLess",
+            [4] = "TimothyMore",
+            [5] = "SeekFaster",
+            [6] = "ScreechFast",
+            [7] = "DupeMost",
+            [8] = "BackdoorVacuum",
+            [9] = "BackdoorLookman",
+            [10] = "NoKeySound",
+            [11] = "Giggle",
+            [12] = "Dread",
+            [13] = "FigureFaster",
+            [14] = "GoldSpawnNone",
+            [15] = "FiredampMost",
+            [16] = "RushMore"
+        },
+        ["Settings"] = {},
+        ["Destination"] = "Mines",
+        ["FriendsOnly"] = false,
+        ["MaxPlayers"] = "12"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("RemotesFolder"):WaitForChild("CreateElevator"):FireServer(unpack(args))
+
+end)
