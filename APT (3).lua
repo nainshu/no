f94fbbf69dfe442a4731a198f1a913ba20051770
nainshu <2087819028@qq.com>
2025-02-13 20:36:43 +0800

    Update APT (3).lua

--- APT (3).lua
+++ APT (3).lua
@@ -12,7 +12,7 @@ print("反挂机开启")
 		   wait(1)
 		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 		end)
-local OrionLib = loadstring(game:HttpGet('https://pastebin.com/raw/SePpsSPZ'))()
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/ChinaQY/-/Main/UI'))()
 local Window = OrionLib:MakeWindow({Name = "APT脚本", HidePremium = false, SaveConfig = true,IntroText = "欢迎使用APT脚本", ConfigFolder = "欢迎APT脚本"})
 local about = Window:MakeTab({
     Name = "脚本名称",
