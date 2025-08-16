dafad196bb4745e8f6d45c2f19d7bc5cfe121998
nainshu <2087819028@qq.com>
2025-08-16 18:19:24 +0800

    Update ori.lua

--- ori.lua
+++ ori.lua
@@ -17,7 +17,7 @@ local OrionLib = {
             Stroke = Color3.fromRGB(0, 0, 0),     
             Divider = Color3.fromRGB(0, 0, 0), 
             Text = Color3.fromRGB(255, 255, 255),  
-            TextDark = Color3.fromRGB(255, 255, 255) 
+            TextDark = Color3.fromRGB(0, 0, 0) 
         }
     },
     SelectedTheme = "Default",
